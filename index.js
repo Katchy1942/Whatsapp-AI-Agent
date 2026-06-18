aaf75ebaf2008e43313e734a1bf4c8d3d72add1a
Katchy1942 <stanleyonahu@gmail.com>
2026-06-18 12:47:13 +0100

    port verification

--- index.js
+++ index.js
@@ -25,4 +25,5 @@ app.post("/webhook", (req, res) => {
 	res.sendStatus(200);
 });
 
-app.listen(3000, () => console.log("Bot running on port 3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Bot running on port ${PORT}`));
