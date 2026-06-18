d4d7d6803ccaec1fa225a8be05e440ee57e83a37
Katchy1942 <stanleyonahu@gmail.com>
2026-06-18 13:15:34 +0100

    fixes

--- index.js
+++ index.js
@@ -3,16 +3,17 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-// Webhook verification
-app.get("/webhook", (req, res) => {
-	console.log("ENV TOKEN:", process.env.VERIFY_TOKEN);
-	console.log("QUERY TOKEN:", req.query["hub.verify_token"]);
+const VERIFY_TOKEN = "7044263024music";
 
+app.get("/webhook", (req, res) => {
+	console.log('ENV TOKEN:', VERIFY_TOKEN);
+	console.log('QUERY TOKEN:', req.query["hub.verify_token"]);
+	
 	const mode = req.query["hub.mode"];
 	const token = req.query["hub.verify_token"];
 	const challenge = req.query["hub.challenge"];
 
-	if (mode === "subscribe" && token === process.env.VERIFY_TOKEN) {
+	if (mode === "subscribe" && token === VERIFY_TOKEN) {
 		res.status(200).send(challenge);
 	} else {
 		res.sendStatus(403);
